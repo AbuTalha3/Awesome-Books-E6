4a348c88065f67dcf92bf9508389d7f98f436f92
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-11 13:14:27 +0500

    Update linter issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import {BookList} from '../modules/bookList';
 import { DateTime } from 'luxon';
+import { BookList } from '../modules/bookList';
 
 const bookList = new BookList();
 bookList.init();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const timer = document.getElementById('datetime')
-  const clock = DateTime.now().toLocaleString()
+  const timer = document.getElementById('datetime');
+  const clock = DateTime.now().toLocaleString();
   timer.textContent = clock;
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,3 @@
 const path = require('path');
-module.exports = {  mode: 'development',  entry: './src/index.js', output: {   filename: 'main.js',  path: path.resolve(__dirname, 'dist'), },};
+
+module.exports = { mode: 'development', entry: './src/index.js', output: { filename: 'main.js', path: path.resolve(__dirname, 'dist') } };
